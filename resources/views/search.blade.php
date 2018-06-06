b1b931d0b9ed22118684d0b081ea3a7e5d585131
allen4dev <alanaliagadev@gmail.com>
2018-06-06 00:18:52 -0400

    [Add] Search page - base markup and design

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,20 +4,92 @@
   <!-- Filters -->
   <section class="Filters">
     <section class="Filter">
-      <h2 class="Filter-title">Search</h2>
+      <!-- Filter-heading -->
+      <header class="Filter-heading">
+        <h2 class="Filter-title">Search</h2>
+      </header>
+      <!-- /Filter-heading -->
 
-      <ul class="Filter-list">
-        <li class="Filter-item">Lara</li>
-      </ul>
+      <!-- Filter-body -->
+      <section class="Filter-body">
+        <form class="Filter-search">
+          <input class="Filter-searchInput" type="text" />
+        </form>
+      </section>
+      <!-- /Filter-body -->
     </section>
 
+    <section class="Filter">
+      <!-- Filter-heading -->
+      <header class="Filter-heading">
+        <h2 class="Filter-title">Skill type</h2>
+      </header>
+      <!-- /Filter-heading -->
 
+      <!-- Filter-body -->
+      <section class="Filter-body">
+        <form class="Filter-tags">
+          <label class="Filter-label">
+            <input class="Filter-tag" type="checkbox" /> Laravel
+          </label>
+
+          <label class="Filter-label">
+            <input class="Filter-tag" type="checkbox" /> Tooling
+          </label>
+
+          <label class="Filter-label">
+            <input class="Filter-tag" type="checkbox" /> JavaScript
+          </label>
+
+          <label class="Filter-label">
+            <input class="Filter-tag" type="checkbox" /> PHP
+          </label>
+
+          <label class="Filter-label">
+            <input class="Filter-tag" type="checkbox" /> Testing
+          </label>
+        </form>
+      </section>
+      <!-- /Filter-body -->
+    </section>
   </section>
   <!-- /Filters -->
 
   <!-- /Results -->
-  <ul class="Search-results">
+  <ul class="Results">
+    <li class="Result">
+      <header class="Result-heading">
+        <h3 class="Result-title">{Thread title}</h3>
+      </header>
 
+      <section class="Result-body">
+        <h4 class="Result-serie">{Thread serie}</h4>
+        <span class="Result-episode">{Thread episode}</span>
+      </section>
+
+      <footer class="Result-footer">
+        <span class="Result-tag">{Serie tag}</span>
+        <span class="Result-date">{Thread creation}</span>
+        <span class="Result-level">{Serie level}</span>
+      </footer>
+    </li>
+
+    <li class="Result">
+      <header class="Result-heading">
+        <h3 class="Result-title">{Thread title}</h3>
+      </header>
+
+      <section class="Result-body">
+        <h4 class="Result-serie">{Thread serie}</h4>
+        <span class="Result-episode">{Thread episode}</span>
+      </section>
+
+      <footer class="Result-footer">
+        <span class="Result-tag">{Serie tag}</span>
+        <span class="Result-date">{Thread creation}</span>
+        <span class="Result-level">{Serie level}</span>
+      </footer>
+    </li>
   </ul>
   <!-- /Results -->
 </section>
