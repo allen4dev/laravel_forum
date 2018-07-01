d7808231e6a312448ce81398c2952149bb537c50
allen4dev <alanaliagadev@gmail.com>
2018-06-30 23:30:08 -0400

    [ToDo] NotificationsTest - an_authenticated_user_is_notified_when_a_subscribed_thread_receives_a_new_reply

---
+++ tests/Feature/NotificationsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Notifications\DatabaseNotification;
+
+use App\Thread;
+use App\User;
+
+class NotificationsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_authenticated_user_is_notified_when_a_subscribed_thread_receives_a_new_reply()
+    {
+        $this->withoutExceptionHandling();
+        // Given we have an authenticated user subscribed to a thread
+        $this->signin(create(User::class));
+        $user = auth()->user();
+
+        $thread = create(Thread::class);
+        
+        $thread->subscribe();
+        
+        $this->signin(create(User::class));
+        // When the thread receives a new reply
+        $thread->addReply([ 'body' => 'Some reply' ]);
+        
+        // Then a new notification should exists in the database
+        $this->assertCount(1, $user->fresh()->notifications);
+    }
+}
